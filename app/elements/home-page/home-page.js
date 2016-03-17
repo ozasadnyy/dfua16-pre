c8a7248836e1821c77725844de566534c994b5e0
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2016-03-17 23:58:48 +0200

    Add animated pages

--- app/elements/home-page/home-page.js
+++ app/elements/home-page/home-page.js
@@ -2,8 +2,19 @@ class HomePage {
   beforeRegister() {
     this.is = 'home-page';
     this.properties = {
-
+      selected: {
+        type: Number,
+        value: 0
+      }
     };
+  }
+
+  ready() {
+    window.clearInterval(_cycleBlocksInterval);
+    var _cycleBlocksInterval = window.setInterval(function () {
+      this.$.blocks.selectNext();
+    }
+      .bind(this), 8000);
   }
 }
 
