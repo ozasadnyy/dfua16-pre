6cc05d9124a2f8d8bf0c053aa533a704f0473b43
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2016-03-23 23:46:23 +0200

    Update design

--- app/elements/home-page/home-page.js
+++ app/elements/home-page/home-page.js
@@ -5,15 +5,32 @@ class HomePage {
       selected: {
         type: Number,
         value: 0
+      },
+      video: {
+        type: Object,
+        value: {
+          youtubeId: 'DfMnJAzOFng'
+        }
+      },
+      mailchimp: {
+        type: Object,
+        value: {
+          name: 'b_b7e853a79164ddfdbda3ed77b_7993e39fbe',
+          url: 'https://gdg.us11.list-manage.com/subscribe/post?u=b7e853a79164ddfdbda3ed77b&amp;id=7993e39fbe'
+        }
       }
     };
   }
 
   ready() {
-    window.clearInterval(_cycleBlocksInterval);
-    var _cycleBlocksInterval = window.setInterval(function () {
-      this.$.blocks.selectNext();
-    }.bind(this), 8000);
+    // window.clearInterval(_cycleBlocksInterval);
+    // var _cycleBlocksInterval = window.setInterval(function () {
+    //   this.$.blocks.selectNext();
+    // }.bind(this), 8000);
+  }
+
+  openVideo() {
+    this.$.videoDialog.open();
   }
 }
 
